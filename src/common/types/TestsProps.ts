bbea21f250ddd77b74fe7e2b1bdbfd2904343a8e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-03 19:16:09 +0300

    create TestsProps.ts added tests props for object

---
+++ src/common/types/TestsProps.ts
@@ -0,0 +1,20 @@
+export type StudentProps = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressProps
+    technologies: TechnologiesProps[]
+}
+type AddressProps = {
+    country: string
+    city: CityProps
+}
+type CityProps = {
+    title: string
+    street: string
+}
+type TechnologiesProps = {
+    id: number
+    title: string
+}
