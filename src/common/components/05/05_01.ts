b28c585d3a8bde1160dd918abe290f669a9fdeff
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-21 01:10:13 +0300

    created 05_01.ts, and implemented code, added functions for tests

---
+++ src/common/components/05/05_01.ts
@@ -0,0 +1,10 @@
+import {GovermentBuildingsProps, HousesProps} from "common/types/TestsProps.ts";
+
+export const getStreetsTitlesOfGovernmentsBuildings = (buildings: GovermentBuildingsProps[]) =>
+    buildings.map(street => street.address.street.title)
+
+export const getStreetsTitlesOfHouses = (houses: HousesProps[]) =>
+    houses.map(house => house.address.street.title);
+
+export const createMessages = (houses: HousesProps[])=>
+    houses.map(house => `Hello guys from ${house.address.street.title}`)
