e0a2c4a7cfee23f9d4fa9e3b908b6e63316086b3
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-20 17:49:04 +0300

    Added new test, the test name: "should take courses chipper 160"

--- src/common/components/04/04.test.ts
+++ src/common/components/04/04.test.ts
@@ -1,4 +1,4 @@
-import {ages, predicate} from "common/components/04/04.ts";
+import {ages, chipPredicate, courses, predicate} from "common/components/04/04.ts";
 
 test("should take old men, older then 90", ()=> {
     const oldAgesWithMethodFilter = ages.filter(age => age > 90);
@@ -8,4 +8,17 @@ test("should take old men, older then 90", ()=> {
     expect(oldAgesWithMethodFilter[0]).toBe(100);
     expect(oldAgesWithOurFunction.length).toBe(1);
     expect(oldAgesWithOurFunction[0]).toBe(100);
+})
+
+test("should take courses chipper 160", ()=> {
+    const chipCoursesWithMethodFilter = courses.filter(course => course.price < 160)
+    const chipCoursesWithOurFunction = courses.filter(chipPredicate)
+
+    expect(chipCoursesWithMethodFilter.length).toBe(2);
+    expect(chipCoursesWithMethodFilter[0].title).toBe("CSS");
+    expect(chipCoursesWithMethodFilter[1].title).toBe("REACT");
+
+    expect(chipCoursesWithOurFunction.length).toBe(2);
+    expect(chipCoursesWithOurFunction[0].title).toBe("CSS");
+    expect(chipCoursesWithOurFunction[1].title).toBe("REACT");
 })
