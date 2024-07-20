11dcba565fd55e4860644caf1eee0547cdd992c8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-21 01:20:17 +0300

    Added code for App.tsx, demonstrated how the map method works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
 
 function App() {
-
+    const names = ['Dimych', 'Sveta', 'Katya', 'Viktor', 'Ignat'];
+    const nameList = names.map((name, index) => (
+        <li key={index}>{name}</li>
+    ));
+    const users = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Sveta"},
+        {id: 3, name: "Katya"},
+        {id: 4, name: "Viktor"},
+        {id: 5, name: "Ignat"},
+    ]
+    const userList = users.map(user => (
+        <li key={user.id}>{user.name}</li>
+    ));
   return (
     <>
-
+      <ul>
+          {nameList}
+      </ul>
+      <ul>
+          {userList}
+      </ul>
     </>
   )
 }
