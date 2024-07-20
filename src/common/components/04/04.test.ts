4cf63974c2673964be426af53febe858f0fcfcbe
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-20 17:49:34 +0300

    Added new test, the test name: "Get only completed tasks"

--- src/common/components/04/04.test.ts
+++ src/common/components/04/04.test.ts
@@ -1,4 +1,4 @@
-import {ages, chipPredicate, courses, predicate} from "common/components/04/04.ts";
+import {ages, chipPredicate, courses, predicate, tasks} from "common/components/04/04.ts";
 
 test("should take old men, older then 90", ()=> {
     const oldAgesWithMethodFilter = ages.filter(age => age > 90);
@@ -21,4 +21,12 @@ test("should take courses chipper 160", ()=> {
     expect(chipCoursesWithOurFunction.length).toBe(2);
     expect(chipCoursesWithOurFunction[0].title).toBe("CSS");
     expect(chipCoursesWithOurFunction[1].title).toBe("REACT");
+})
+
+test("Get only completed tasks", ()=> {
+    const completedTasks = tasks.filter(task => task.isDone)
+
+    expect(completedTasks.length).toBe(2);
+    expect(completedTasks[0].id).toBe(2);
+    expect(completedTasks[1].id).toBe(4);
 })
