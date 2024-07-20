cb46afa2301ef47af19b74434c6a4e24fe85272d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-21 01:08:09 +0300

    Created 05_01.test.ts for tests, and added test, the test name: "list of streets titles of governments buildings"

---
+++ src/common/components/05/05_01.test.ts
@@ -0,0 +1,76 @@
+import {CityProps} from "common/types/TestsProps.ts";
+import {getStreetsTitlesOfGovernmentsBuildings} from "common/components/05/05_01.ts";
+
+let city: CityProps
+
+beforeEach(() => {
+    city = {
+        title: "New York",
+        houses: [
+            {
+                id: 1,
+                buildedAt: 2012,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: "White street"
+                    }
+                }
+            },
+            {
+                id: 2,
+                buildedAt: 2008,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            },
+            {
+                id: 3,
+                buildedAt: 2020,
+                repaired: false,
+                address: {
+                    number: 101,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            }
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 2000,
+                address: {
+                    street: {
+                        title: "Central Str"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 50000,
+                staffCount: 200,
+                address: {
+                    street: {
+                        title: "South Str"
+                    }
+                }
+            },
+        ],
+        citizensNumber: 1000000
+    }
+})
+
+test("list of streets titles of governments buildings", ()=> {
+    let streetsNames = getStreetsTitlesOfGovernmentsBuildings(city.governmentBuildings);
+
+    expect(streetsNames.length).toBe(2);
+    expect(streetsNames[0]).toBe("Central Str");
+    expect(streetsNames[1]).toBe("South Str");
+})
