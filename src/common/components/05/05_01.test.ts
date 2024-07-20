747443592d2e3b8a550d8fb4951b8661ffbe0e2d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-21 01:08:57 +0300

    Added new test, the test name: "create greeting messages or streets"

--- src/common/components/05/05_01.test.ts
+++ src/common/components/05/05_01.test.ts
@@ -87,3 +87,12 @@ test("list of streets titles", ()=> {
     expect(streets[1]).toBe("Happy street");
     expect(streets[2]).toBe("Happy street");
 })
+
+test("create greeting messages or streets", ()=> {
+    let messages = createMessages(city.houses)
+
+    expect(messages.length).toBe(3);
+    expect(messages[0]).toBe("Hello guys from White street")
+    expect(messages[1]).toBe("Hello guys from Happy street")
+    expect(messages[2]).toBe("Hello guys from Happy street")
+})
