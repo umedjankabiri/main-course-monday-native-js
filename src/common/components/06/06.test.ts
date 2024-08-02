0ba20a02e75a5ac212ddf8ee203f5df1b7a491aa
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-02 17:41:08 +0300

    The 06.test.ts file for tests has been created, and the code has been implemented, added test, the test name is "Object should be destructured"

---
+++ src/common/components/06/06.test.ts
@@ -0,0 +1,31 @@
+import {ManTestProps} from "common/types/ManTestProps.ts";
+let props: ManTestProps
+
+beforeEach(() => {
+    props = {
+        name: "Umedjan",
+        age: 32,
+        lessons: [{title: '1'}, {title: '2'}, {title: '3', name: "react"}],
+        address: {
+            street: {
+                title: 'Selim sk.'
+            }
+        }
+    }
+})
+
+test("Object should be destructured", () => {
+    // const age = props.age
+    // const lessons = props.lessons
+    const a = props.age
+    const l = props.lessons
+
+    const {age, lessons} = props
+    const {title} = props.address.street
+
+    expect(age).toBe(32);
+    expect(lessons.length).toBe(3);
+    expect(a).toBe(32);
+    expect(l.length).toBe(3);
+    expect(title).toBe('Selim sk.');
+})
